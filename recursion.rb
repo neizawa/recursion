c2e4aae50ef4edc9def863797ccc823bfdf2625d
neizawa <meeteori@gmail.com>
2022-11-05 20:06:50 +0500

    Add Fibonacci method using recursion

--- recursion.rb
+++ recursion.rb
@@ -1,7 +1,11 @@
-def fib(num)
+def fibs(num)
   arr = [0, 1]
   (num - 2).times { arr << arr[-1] + arr[-2] }
   arr
 end
 
-p fib(8)
+def fibs_rec(num)
+  num < 2 ? num : fibs_rec(num - 1) + fibs_rec(num - 2)
+end
+
+p fibs_rec(8)
