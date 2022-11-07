49204224c07cf9af86bd4ebd7662672ae2f6ba76
neizawa <meeteori@gmail.com>
2022-11-07 19:12:43 +0500

    Add merge_sort method

--- recursion.rb
+++ recursion.rb
@@ -8,4 +8,30 @@ def fibs_rec(num)
   num < 2 ? num : fibs_rec(num - 1) + fibs_rec(num - 2)
 end
 
-p fibs_rec(8)
+def merge_sort(arr)
+  if arr.size == 1
+    arr
+  else
+    left = merge_sort(arr[0..((arr.length / 2) - 1)])
+    right = merge_sort(arr[(arr.length / 2)..])
+    sorted = []
+
+    until left.empty? || right.empty?
+      if left.first < right.first
+        sorted << left.shift
+      else
+        sorted << right.shift
+      end
+    end
+
+    if left.empty?
+      right.each { |el| sorted << el }
+    elsif right.empty?
+      left.each { |el| sorted << el }
+    end
+
+    sorted
+  end
+end
+
+p merge_sort([0, 32_423, 123, 67, 727, 9, 1, 0, 0, 4534, 69])
